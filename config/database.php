fae172a4930aac3e4c335ee70a47a8ec776100a4
AgbobaKehinde <Gideonju1738@gmail.com>
2018-05-14 15:07:34 +0100

    Database setup

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
 
 $url = parse_url(getenv("DATABASE_URL"));
-$host = $url["host"]?? null;
-$username = $url["user"]?? null;
-$password = $url["pass"]?? null;
-$database = substr($url["path"], 1)?? null;
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 
 return [
 
