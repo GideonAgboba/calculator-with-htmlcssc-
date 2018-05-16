baee4465e7f2301383127439671bc171996470c2
AgbobaKehinde <Gideonju1738@gmail.com>
2018-05-16 12:50:40 +0100

    gg

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
-Route::get('/search', 'HomeController@search')
+Route::get('/search', 'HomeController@search');
 
 
 Route::group(['middleware' => ['web']], function(){
