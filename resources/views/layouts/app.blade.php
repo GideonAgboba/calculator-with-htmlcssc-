93906ab23bff4f1b51ddc10ef5239a2df6889401
AgbobaKehinde <Gideonju1738@gmail.com>
2018-05-18 02:47:02 +0100

    mobile responsiveness

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -94,7 +94,7 @@
     </script>
     <script type="text/javascript">
         if (screen.width <= 400) {
-            window.location = "<?php url('/indexmobile'); ?>";
+            window.location = "/indexmobile";
         }
     </script>
 </head>
