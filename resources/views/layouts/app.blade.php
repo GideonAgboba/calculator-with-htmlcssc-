9db4f9629d606c9253364a871549e1e742e87534
AgbobaKehinde <Gideonju1738@gmail.com>
2018-05-20 13:43:01 +0100

    index update for form routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -100,6 +100,36 @@
     </script>
     <style>
         
+/* ------------------------------------------------------------------- 
+ * ## pace.js styles - minimal
+ * ------------------------------------------------------------------- */
+.pace {
+    -webkit-pointer-events: none;
+    pointer-events: none;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    user-select: none;
+}
+
+.pace-inactive {
+    display: none;
+}
+
+.pace .pace-progress {
+    background: #39b54a;
+    position: fixed;
+    z-index: 900;
+    top: 0;
+    right: 100%;
+    width: 100%;
+    height: 4px;
+}
+
+.oldie .pace {
+    display: none;
+}
+
+
 /* ===================================================================
  * # preloader
  *
