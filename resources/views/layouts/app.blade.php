1eb74cdd35f746fd31d48fab4958898ccbe9281f
AgbobaKehinde <Gideonju1738@gmail.com>
2018-05-20 13:48:11 +0100

    index update for form routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -98,6 +98,11 @@
         }
 
     </script>
+
+<!-- script
+    ================================================== -->
+    <script src="mobile/js/modernizr.js"></script>
+    <script src="mobile/js/pace.min.js"></script>
     <style>
         
 /* ------------------------------------------------------------------- 
@@ -243,10 +248,6 @@
   </div>
 </div>
 @endif
-<!-- script
-    ================================================== -->
-    <script src="mobile/js/modernizr.js"></script>
-    <script src="mobile/js/pace.min.js"></script>
     @yield('contents')
     <div id="preloader">
         <div id="loader">
